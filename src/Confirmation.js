176592fc217e0f33c19df05921ad8fa93b0f04f2
AdamI19 <broadscars@gmail.com>
2021-09-17 10:35:40 -0600

    debugging

---
+++ src/Confirmation.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Confirmation({ details })
+{
+    if (!details)
+    {
+        return <h3>Working on your order&apos;s details...</h3>;
+    }
+
+    return (
+        <div className='form-group submit'>
+            <h2>{details.customerName}'s Order Details</h2>
+
+            <div className='form-group inputs'>
+                <p>Pizza Size: {details.pizzaSize}</p>
+
+                {
+                    !!details.toppings && !!details.toppings.length &&
+                    <div>
+                        Toppings:
+                        <ul>
+                            {details.toppings.map((topping, idx) => <li key={idx}>{topping}</li>)}
+                        </ul>
+                    </div>
+                }
+
+                <p>Special Instruction: {details.special}</p>
+                <img src={pizza1} alt="Dog and cat with Pizza Pic" />
+            </div>
+        </div>
+    );
+}
+
+export default Confirmation; 
